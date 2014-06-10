7f2c7a990d6a3c72ae4d518ac813fbfa4708e0a4
JennyPaola <jennyk_94@hotmail.com>
2014-06-10 15:32:48 -0500

    cree loso atributos de genero

--- Karaoke/src/logica/Genero.java
+++ Karaoke/src/logica/Genero.java
@@ -1,5 +1,37 @@
 package logica;
 
+import java.net.URL;
+import java.util.ArrayList;
+
 public class Genero {
+	
+	private String nombre ;
+	private ArrayList<Cancion> listaCancion ;
+	private String imagen ;
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public ArrayList<Cancion> getListaCancion() {
+		return listaCancion;
+	}
+	public void setListaCancion(ArrayList<Cancion> listaCancion) {
+		this.listaCancion = listaCancion;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	
+	
+	
 
 }
