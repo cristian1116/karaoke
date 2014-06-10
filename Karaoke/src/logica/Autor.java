c26560b99c54b7e39984b3baede00825ffae45e3
JennyPaola <jennyk_94@hotmail.com>
2014-06-10 15:50:44 -0500

    cree atributos  getters and setters

--- Karaoke/src/logica/Autor.java
+++ Karaoke/src/logica/Autor.java
@@ -1,5 +1,34 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Autor {
+	
+	private String nombre ;
+	private String imagen ;
+	private ArrayList<Cancion> listaCanciones ;
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public ArrayList<Cancion> getListaCanciones() {
+		return listaCanciones;
+	}
+	public void setListaCanciones(ArrayList<Cancion> listaCanciones) {
+		this.listaCanciones = listaCanciones;
+	}
+	
 
 }
