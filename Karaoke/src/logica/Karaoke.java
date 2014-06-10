42a7e1dcce5352643ff09874fa41469139438536
JennyPaola <jennyk_94@hotmail.com>
2014-06-10 15:37:23 -0500

    cree atributos de la clase karaoke

--- Karaoke/src/logica/Karaoke.java
+++ Karaoke/src/logica/Karaoke.java
@@ -1,5 +1,30 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Karaoke {
+	
+	private ArrayList<Genero> listaGeneros ;
+	private Cancion cancionActual;
+	
+	
+	
+	
+	
+	public ArrayList<Genero> getListaGeneros() {
+		return listaGeneros;
+	}
+	public void setListaGeneros(ArrayList<Genero> listaGeneros) {
+		this.listaGeneros = listaGeneros;
+	}
+	public Cancion getCancionActual() {
+		return cancionActual;
+	}
+	public void setCancionActual(Cancion cancionActual) {
+		this.cancionActual = cancionActual;
+	}
+	
+	
+	
 
 }
