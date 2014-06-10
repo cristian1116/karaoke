b49877cff90b6ca3898617666b8bd8b3a0b629ed
cristian <monsalve70@hotmail.com>
2014-06-10 15:40:57 -0500

    realice los atributos de cancion

--- Karaoke/src/logica/Cancion.java
+++ Karaoke/src/logica/Cancion.java
@@ -1,5 +1,51 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Cancion {
+	private String nombre;
+	private int  duracion;
+	private int numeroLineaActual;
+	private String imagen;
+	private ArrayList<Cancion> letracancion;
+
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public int getNumeroLineaActual() {
+		return numeroLineaActual;
+	}
+	public void setNumeroLineaActual(int numeroLineaActual) {
+		this.numeroLineaActual = numeroLineaActual;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public ArrayList<Cancion> getLetracancion() {
+		return letracancion;
+	}
+	public void setLetracancion(ArrayList<Cancion> letracancion) {
+		this.letracancion = letracancion;
+	}
+	
+	
+	 
+	
+	
+	
+	
 
 }
