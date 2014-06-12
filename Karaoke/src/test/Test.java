5da10d570f41bbeed7fb3107e5e74c8c89891258
SALA <SALA@acwin7c126-11.uptc.edu.co>
2014-06-12 15:09:11 -0500

    hice test

--- Karaoke/src/test/Test.java
+++ Karaoke/src/test/Test.java
@@ -1,10 +1,13 @@
 package test;
 
+import presentacion.VentanaPrincipal;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+		ventanaPrincipal.setVisible(true);
 	}
 
 }
